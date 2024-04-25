8388d82ee8ee4c031f672b54a6d822fd554b4eec
sam <jialin.hou@deeplang.ai>
2024-04-25 09:14:03 +0800

    feat: create vite configure

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+// function bootstrap() {
+  const root = createRoot(document.getElementById('app'));
+  root.render(<App />);
+// }
+
+// bootstrap();
