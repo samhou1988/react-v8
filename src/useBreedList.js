e3e2f5ef95aa8764a0f98a86a43583bb1ab52b6d
sam <jialin.hou@deeplang.ai>
2024-04-25 10:20:02 +0800

    feat: react query

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,29 +1,12 @@
 import { useState, useEffect } from 'react';
+import fetchBreedList from "./fetchBreedList";
 
 const localCache = {};
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery({
+    queryKey: ["breedList", animal],
+    queryFn: fetchBreedList,
+  });
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList(animal);
-    }
-  }, [animal]);
-
-  async function requestBreedList(animal) {
-    setBreedList([]);
-    setStatus("loading");
-    const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
-    const json = await res.json();
-    localCache[animal] = json.breeds || [];
-    setBreedList(localCache[animal]);
-    setStatus("loaded");
-  }
-
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], results.status];
 }
